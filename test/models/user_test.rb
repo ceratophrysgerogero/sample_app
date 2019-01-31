b42a4986282d07958a67e9371fba8c7a551cb8fd
ceratophrysgerogero <ceratophrysgerogero@gmail.com>
2019-01-31 23:57:39 +0900

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -75,5 +75,12 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
     end
+    # ランダムなトークンを返す
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
 
+    test "authenticated? should return false for a user with nil digest" do
+  assert_not @user.authenticated?('')
+end
 end
